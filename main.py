2267953dd2ff9c4c59e1d5765636ea3e4815206a
Adick2 <adick@connect.kellettschool.com>
2024-10-07 07:09:37 +0000

    idk

---
+++ main.py
@@ -0,0 +1,23 @@
+from time import sleep
+
+for i in range(10):
+    print("💃💃💃💃💃💃💃💃💃💃💃💃💃💃💃💃💃🌮🌮🌮🌮🌮🌮🌮🌮🌮🌮🌮🌮🌮🌮🌮🌮🌮✊✊✊✊✊✊✊✊✊✊✊✊✊✊✊✊✊✊")
+sleep(3)
+print ("Welcome💃,")
+sleep(1)
+print("to🌮")
+sleep(1)
+print("the✊")
+sleep(1)
+print("dancing💃💃")
+sleep(1)
+print("taco🌮🌮")
+sleep(1)
+print("revolution✊✊!!!!!!!")
+print("Here is our latest follower:JOSEPH🤓.🕺🕺🕺🕺")
+
+
+
+
+
+
